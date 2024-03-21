bacb5a4d8bf70ed924913a139fb2a3fd79872c37
lkelly01 <laurynkelly55@gmail.com>
2024-03-21 15:22:05 -0400

    changes the movement so charcter doesnt not have buggy movement

--- Assets/Scripts/MainCharMovement.cs
+++ Assets/Scripts/MainCharMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,45 +9,58 @@ public class PlayerController : MonoBehaviour
 
     private bool isMoving;
     private Vector2 input;
-
     public LayerMask solidObjectsLayer;
     public LayerMask interactableLayer;
 
     private Rigidbody2D rigidBody2D;
 
-    private void Start(){
+    private void Start()
+    {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        
     }
 
     private void Update()
     {
-       
-
         if (!isMoving)
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            // Instead of accessing "moveX" and "moveY", you can directly use input.x and input.y
+            var facingDir = new Vector3(input.x, input.y);
+
             if (input != Vector2.zero)
             {
-                Vector3 targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
+                Vector3 targetPos = transform.position + new Vector3(input.x, input.y, 0f);
 
-                if(IsWalkable(targetPos)){
-
-                
-                 StartCoroutine(Move(targetPos));
+                if (IsWalkable(targetPos))
+                {
+                    StartCoroutine(Move(targetPos));
+                    // Set IsRunning parameter in the Animator
+                    Animator.SetBool("IsRunning", true);
                 }
-                // Set IsRunning parameter in the Animator
-                Animator.SetBool("IsRunning", true);
             }
             else
             {
                 // Set IsRunning parameter to false when not moving
                 Animator.SetBool("IsRunning", false);
             }
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+            Interact();
+    }
+
+    void Interact()
+    {
+        var facingDir = new Vector3(input.x, input.y);
+        var interactPos = transform.position + facingDir;
+        // Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
+
+        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
+
+        if (collider != null)
+        {
+            Debug.Log("there is an NPC here! ");
         }
     }
 
@@ -56,8 +70,8 @@ public class PlayerController : MonoBehaviour
 
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            rigidBody2D.MovePosition(targetPos);
-            //transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            rigidBody2D.MovePosition(newPosition);
             yield return null;
         }
 
@@ -65,12 +79,10 @@ public class PlayerController : MonoBehaviour
 
         isMoving = false;
     }
+
     private bool IsWalkable(Vector3 targetPos)
     {
-            if(Physics2D.OverlapCircle(targetPos,0.2f,solidObjectsLayer | interactableLayer) != null)
-            {
-                return false;
-            }
-            return true;
+        Collider2D collider = Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer);
+        return (collider == null);
     }
 }
