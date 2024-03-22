d06b9f1443970dddca512f3cb97d929f2c9fb18f
Ryan-Segali <rsegali415@gmail.com>
2024-03-22 14:37:36 -0400

    added menu

---
+++ Assets/exit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitG : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
