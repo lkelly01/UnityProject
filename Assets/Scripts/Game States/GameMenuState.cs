69827293c5c775e0422684ff034513a88d766088
Ryan-Segali <rsegali415@gmail.com>
2024-04-18 12:18:40 -0400

    Totally revamped using grid layout
    I restarted the project using tutorials now game is based on a grid with game additively using sceneManager fully functional battle system with criticals status effects stat adjusting moves. Main thing to be added is to finish the map and make final boss that needs 3 "gym badges" to actually fight

---
+++ Assets/Scripts/Game States/GameMenuState.cs
@@ -0,0 +1,49 @@
+using GDEUtils.StateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuState : State<GameController>
+{
+    [SerializeField] MenuController menuController;
+
+    public static GameMenuState i { get; private set; }
+    private void Awake()
+    {
+        i = this;
+    }
+
+    GameController gc;
+    public override void Enter(GameController owner)
+    {
+        gc = owner;
+        menuController.gameObject.SetActive(true);
+        menuController.OnSelected += OnMenuItemSelected;
+        menuController.OnBack += OnBack;
+    }
+
+    public override void Execute()
+    {
+        menuController.HandleUpdate();
+    }
+
+    public override void Exit()
+    {
+        menuController.gameObject.SetActive(false);
+        menuController.OnSelected -= OnMenuItemSelected;
+        menuController.OnBack -= OnBack;
+    }
+
+    void OnMenuItemSelected(int selection)
+    {
+        if (selection == 0) // Pokemon
+            gc.StateMachine.Push(PartyState.i);
+        else if (selection == 1) // Bag
+            gc.StateMachine.Push(InventoryState.i);
+    }
+
+    void OnBack()
+    {
+        gc.StateMachine.Pop();
+    }
+}
